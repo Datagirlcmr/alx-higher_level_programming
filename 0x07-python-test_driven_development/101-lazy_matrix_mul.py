941aaab556f320c366aa4d015b303d412258f72b
Datagirl <datagirlcmr@gmail.com>
2023-08-11 06:44:36 +0100

    fix failing test

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-"""Module lazy_matrix_mul
-Matrix multiplication using NumPy.
 """
-import numpy
+Module composed by a function that multiplies 2 matrices
+"""
+import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Multiplies m_a and m_b using
-    matmul, and returns the result.
+    """Return the multiplication of two matrices.
+
+    Args:
+        m_a: matrix a
+        m_b: matrix b
+
+    Returns:
+        result of the multiplication
     """
-    return numpy.matmul(m_a, m_b)
+
+    return (np.matmul(m_a, m_b))
